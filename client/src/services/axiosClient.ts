4dcae425cbe51fb4c642e1116ed0bca35cf12d2f
Coding Lover -Asikur <141431875+Md-Asikur-Rahman2@users.noreply.github.com>
2024-01-15 22:46:18 +0600

    Update axiosClient.ts

--- client/src/services/axiosClient.ts
+++ client/src/services/axiosClient.ts
@@ -6,7 +6,7 @@ import { cookies } from "../utils";
 export const axiosClient = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? `https://chatvia-server.vercel.app/api`
+      ? `https://chatvia-demo-server.onrender.com/api`
       : `http://localhost:5000/api`,
   headers: {
     "Content-Type": "application/json",
